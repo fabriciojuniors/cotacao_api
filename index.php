e4ce92da00c68f1ce53d1d7a25520f4483ce0b4c
Junior Souza <fabriciojuniorsc@gmail.com>
2021-03-13 14:55:08 -0300

    Alterações

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
-echo "oi";
+    include $_SERVER['DOCUMENT_ROOR'] . '/cotacao_api/configs/database.php';
+
+    $conexao = new Database();
+    $conexao = $conexao->getConnection();
+
+    echo $conexao;
