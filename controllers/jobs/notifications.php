9ded75d006223929278f531e67299b0e338013f3
Junior Souza <fabriciojuniorsc@gmail.com>
2021-03-26 13:55:05 -0300

    Ajuste nos links entre as paginas

--- controllers/jobs/notifications.php
+++ controllers/jobs/notifications.php
@@ -1,6 +1,6 @@
 <?php
     include 'email.php';
-    include_once $_SERVER['DOCUMENT_ROOT'].'/configs/database.php';
+    include_once '../../configs/database.php';
 
     $conexao = new Database();
     $conexao = $conexao->getConnection();
